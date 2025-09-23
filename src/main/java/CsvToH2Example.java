87a8f6c29cbdf050579015601619f0b33c88ae10
Sumi Tharayil Surendran <tharayilsurendransumi@gmail.com>
2025-09-23 17:11:49 +0100

    Add logic to count the number of rows from  movies table

--- src/main/java/CsvToH2Example.java
+++ src/main/java/CsvToH2Example.java
@@ -6,56 +6,66 @@ import java.math.BigDecimal;
 import java.sql.*;
 
 public class CsvToH2Example {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("*****Create table inside H2 Database****");
-        String libcurl ="jdbc:h2:mem:test5";
-        String user ="sa";
-        String password="";
-        try{
-            Connection connection = DriverManager.getConnection(libcurl,user,password);
+        String libcurl = "jdbc:h2:mem:test5";
+        String user = "sa";
+        String password = "";
+        try {
+            Connection connection = DriverManager.getConnection(libcurl, user, password);
             System.out.println("Connection is created");
             Statement statement = connection.createStatement();
-            String sql ="CREATE TABLE movies("+
-                    "Film VARCHAR(255),"+
-                    "Genre VARCHAR(255),"+
-                    "Lead_studio VARCHAR(255),"+
-                    "Audience_Score_pc INT,"+ "Profitability DECIMAL(10,2),"+ "Rotten_Tomatoes INT,"+
-                    "Worldwide_Gross DECIMAL(15,2),"+ "Release_Year INT )";
+            String sql = "CREATE TABLE movies(" +
+                    "Film VARCHAR(255)," +
+                    "Genre VARCHAR(255)," +
+                    "Lead_studio VARCHAR(255)," +
+                    "Audience_Score_pc INT," + "Profitability DECIMAL(10,2)," + "Rotten_Tomatoes INT," +
+                    "Worldwide_Gross DECIMAL(15,2)," + "Release_Year INT )";
 
             statement.execute(sql);
             System.out.println("Table created");
         } catch (SQLException e) {
-            System.out.println( e.getMessage());
+            System.out.println(e.getMessage());
         }
-        try(CSVReader reader = new CSVReader(new InputStreamReader
+        try (CSVReader reader = new CSVReader(new InputStreamReader
                 (CsvToH2Example.class.getResourceAsStream("data.csv")))) {
-            System.out.println("its reading");Connection connection = DriverManager.getConnection(libcurl,user,password);
-            String insertsql = "INSERT INTO movies(Film,Genre,Lead_Studio,Audience_Score_pc,Profitability,"+
-                    "Rotten_Tomatoes,Worldwide_Gross,Release_Year)"+"VALUES(?,?,?,?,?,?,?,?)";
+            System.out.println("its reading");
+            Connection connection = DriverManager.getConnection(libcurl, user, password);
+            String insertsql = "INSERT INTO movies(Film,Genre,Lead_Studio,Audience_Score_pc,Profitability," +
+                    "Rotten_Tomatoes,Worldwide_Gross,Release_Year)" + "VALUES(?,?,?,?,?,?,?,?)";
             PreparedStatement ptst = connection.prepareStatement(insertsql);
-            String [] line;
+            String[] line;
             reader.readNext();
-            while((line=reader.readNext())!= null){
-                ptst.setString(1,line[0]);
-                ptst.setString(2,line[1]);
-                ptst.setString(3,line[2]);
-                ptst.setInt(4,Integer.parseInt(line[3]));
-                ptst.setBigDecimal(5,new BigDecimal(line[4]));
-                ptst.setInt(6,Integer.parseInt(line[5]));
-                ptst.setBigDecimal(7,new BigDecimal(line[6])) ;
-                ptst.setInt(8,Integer.parseInt(line[7]));
+            while ((line = reader.readNext()) != null) {
+                ptst.setString(1, line[0]);
+                ptst.setString(2, line[1]);
+                ptst.setString(3, line[2]);
+                ptst.setInt(4, Integer.parseInt(line[3]));
+                ptst.setBigDecimal(5, new BigDecimal(line[4]));
+                ptst.setInt(6, Integer.parseInt(line[5]));
+                ptst.setBigDecimal(7, new BigDecimal(line[6]));
+                ptst.setInt(8, Integer.parseInt(line[7]));
                 ptst.addBatch();
 
             }
             ptst.executeBatch();
             System.out.println("CSV file loaded to H2 databse Successfully");
+            String rows = "SELECT COUNT(*)FROM movies";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(rows);
+
+            if (resultSet.next()) {
+                int count = resultSet.getInt(1);
+
+                System.out.println("My table has" + count);
+            }
 
 
-
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
 
     }
+
 }
